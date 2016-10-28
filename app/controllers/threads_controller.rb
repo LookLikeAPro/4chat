2d294db9280ae0e8ecfc463b93d2d1120c2fb78a
looklikeapro <jerryzhou00@gmail.com>
2016-10-27 20:48:46 -0400

    threads template

--- app/controllers/threads_controller.rb
+++ app/controllers/threads_controller.rb
@@ -2,7 +2,7 @@ class ThreadsController < ApplicationController
 	def index
 		# @threads = Comment.where(is_post: true)
 		# @threads = @topic.posts.page(params[:page]).per(2)
-		@threads = Comment.where("post_id IS NULL").page(params[:page]).per(20)
+		@threads = Comment.threads.page(params[:page]).per(20)
 		# paginate json: threads.as_json(
 		# 	# :only => [:title, :description, :author,],
 		# 	# :methods => [:picture_medium, :friendly_id]
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,12 @@
+class String
+  def integer?
+    Integer(self)
+    return true
+  rescue ArgumentError
+    return false
+  end
+end
+
 class Comment < ApplicationRecord
 	belongs_to :comment
 	has_many :comments, :class_name => "Comment", :foreign_key => "post_id"
@@ -5,10 +14,12 @@ class Comment < ApplicationRecord
 	def textMarkup
 		ret = []
 		self.text.split("\n").each do |line|
-			if line[0] == '>'
-				ret.push "<span class='quote'>"+line+"</span>"
+			if line[0..1] == '>>' && line[2..9].integer?
+				ret.push "<a href='##{line[2..9]}' class='quotelink'>#{line}</a>"
+			elsif line[0] == '>'
+				ret.push "<span class='quote'>#{line}</span>"
 			else
-				ret.push "<span>"+line+"</span>"
+				ret.push "<span>#{line}</span>"
 			end
 		end
 		ret.join("<br />")
